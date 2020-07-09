9fe3de98301f3c3bd99fedb6d3307b5032e182bb
Simon <hello@13thontheleft.xyz>
2020-07-09 10:45:16 +0100

    working version

---
+++ lambda/constants.js
@@ -0,0 +1,17 @@
+/* CONSTANTS */
+
+exports.skill = {
+  appId: 'amzn1.ask.skill.7cc90fa6-a609-463f-b9bd-bf8958e60b11',
+  dynamoDBTableName: 'Audio-Player-Multi-Stream',
+};
+
+exports.audioData = [
+  {
+    title: 'Episode 139',
+    url: 'https://peppa-pig-alexa-assets.s3.amazonaws.com/1.0.0/storyaudio/peppaGND.mp3',
+  },
+  {
+    title: 'Episode 140',
+    url: 'https://peppa-pig-alexa-assets.s3.amazonaws.com/1.0.0/storyaudio/peppaMBNR.mp3',
+  },
+];
